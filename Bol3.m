350fe8b66fb0ac0d4e4d3c1d90492a23b6b38eb8
93xiaoming <39090764+93xiaoming@users.noreply.github.com>
2018-05-10 21:14:32 +0800

    Delete Bol3.m

--- Bol3.m
+++
@@ -1,6 +0,0 @@
-%given a rotation R(theta,psi,phi), determine whether it can be decomposed
-%as Rz(*)Rm(*)Rz(*).
-function Bol = Bol3( theta,psi,phi,Theta,Phi )
-    Bol=abs(sin(theta)*sin(phi/2))<=abs(sin(Theta));
-end
-
